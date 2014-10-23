9632bc6d7a1870ff5bfc9af6076a3092f9cb2395
Florian Schneider <florian.schneider@student.tugraz.at>
2014-10-23 14:19:28 +0200

    added operator== in flags and added a README.md

--- Flag.cpp
+++ Flag.cpp
@@ -5,11 +5,20 @@ Flag::Flag(std::string flag, char shortcut) : name_(flag), shortcut_(shortcut)
 
 bool Flag::operator==(const char to_compare)
 {
-  if()
+  if(shortcut_ == to_compare)
+  {
+    return true;
+  }
+
   return false;
 }
 
 bool Flag::operator==(const std::string to_compare)
 {
+  if(name_ == to_compare)
+  {
+    return true;
+  }
+  
   return false;
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+pfix should be a simple shell binary to convert a given mathematical term written
+in infix form to his postfix form
+
+further descriptions will be added
